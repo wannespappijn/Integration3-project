77365b8a3d694bd0f0b5aa9212ca06cb708de7ca
wannespappijn <wannes.pappijn@student.howest.be>
2023-01-29 21:48:25 +0100

    added audio stopping when clicking other

--- src/js/scripts/audio.js
+++ src/js/scripts/audio.js
@@ -1,60 +1,33 @@
+let currentAudio;
+let currentButton;
+let currentImage;
 
-const $rebecAudio = document.querySelector(".audio-rebec");
-const $rebecPlayButton = document.querySelector(".play-button-rebec");
-const $rebecImage = document.querySelector(".image-rebec");
+const playAudio = (audio, playButton, image) => {
+    const $audio = document.querySelector(audio);
+    const $playButton = document.querySelector(playButton);
+    const $image = document.querySelector(image);
 
-console.log($rebecAudio);
-console.log($rebecPlayButton);
+    $playButton.addEventListener("click", function () {
+        if (currentAudio === $audio && currentButton === $playButton && currentImage === $image) {
+            $audio.pause();
+            $playButton.classList.remove("spin");
+            $image.classList.remove("wiggle");
+        } else {
+            if (currentAudio) {
+                currentAudio.pause();
+                currentButton.classList.remove("spin");
+                currentImage.classList.remove("wiggle");
+            }
+            $audio.play();
+            $playButton.classList.add("spin");
+            $image.classList.add("wiggle");
+            currentAudio = $audio;
+            currentButton = $playButton;
+            currentImage = $image;
+        }
+    });
+}
 
-$rebecPlayButton.addEventListener("click", function () {
-    if ($rebecAudio.paused) {
-        $rebecAudio.play();
-        $rebecPlayButton.classList.add("spin");
-        $rebecImage.classList.add("wiggle");
-    } else {
-        $rebecAudio.pause();
-        $rebecPlayButton.classList.remove("spin");
-        $rebecImage.classList.remove("wiggle");
-    }
-});
-
-
-const $rebabAudio = document.querySelector(".audio-rebab");
-const $rebabPlayButton = document.querySelector(".play-button-rebab");
-const $rebabImage = document.querySelector(".image-rebab");
-
-console.log($rebabAudio);
-console.log($rebabPlayButton);
-
-$rebabPlayButton.addEventListener("click", function () {
-    if ($rebabAudio.paused) {
-        $rebabAudio.play();
-        $rebabPlayButton.classList.add("spin");
-        $rebabImage.classList.add("wiggle");
-    } else {
-        $rebabAudio.pause();
-        $rebabPlayButton.classList.remove("spin");
-        $rebabImage.classList.remove("wiggle");
-    }
-});
-
-
-
-const $lyraAudio = document.querySelector(".audio-lyra");
-const $lyraPlayButton = document.querySelector(".play-button-lyra");
-const $lyraImage = document.querySelector(".image-lyra");
-
-console.log($lyraAudio);
-console.log($lyraPlayButton);
-
-$lyraPlayButton.addEventListener("click", function () {
-    if ($rebabAudio.paused) {
-        $lyraAudio.play();
-        $lyraPlayButton.classList.add("spin");
-        $lyraImage.classList.add("wiggle");
-    } else {
-        $lyraAudio.pause();
-        $lyraPlayButton.classList.remove("spin");
-        $lyraImage.classList.remove("wiggle");
-    }
-});
+playAudio(".audio-rebec", ".play-button-rebec", ".image-rebec");
+playAudio(".audio-rebab", ".play-button-rebab", ".image-rebab");
+playAudio(".audio-lyra", ".play-button-lyra", ".image-lyra");
